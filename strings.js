122b59689c152cb75d266ef7f65a10f55d2a86d9
linetmusembi <musembilinetmwongeli@gmail.com>
2023-03-10 19:11:05 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,27 @@
+// Extract the first four characters from the string below;
+// "beautiful"
+let word = "beautiful";
+console.log(word[0]+word[1]+word[2]+word[3]);
+// Insert the following string at the tenth index of the below variable:
+// "eat"
+// const food = "I did not have appetite today"
+
+const food = "I did not have appetite today";
+console.log(`${food[10]}eat`);
+// Count how many times the following string appears in the string variable:
+// 1. "the"
+// 2."s"
+// const story= "She sells sea shells at the sea shore"
+const story = "She sells sea shells at the sea shore"
+
+
+// Convert the following strings into the specified format:
+// 1. UpperCase: "CONfidant"
+// 2. LowerCase: "amazing", "beautiFUL"
+// 3. Title case "A busy office"
+let b = "CONfidant"
+let d = b.toUpperCase();
+console.log(b.toUpperCase());
+
+let name1 = "amazing";
+let name2 = "beautiFUL"
